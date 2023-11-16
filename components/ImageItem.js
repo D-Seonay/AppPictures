192dfb59cc3cae754622534fbb18cd268c427aef
Mathéo DELAUNAY <dseonay@gmail.com>
2023-11-16 17:19:12 +0100

    Refactor ImageItem and Search components

--- components/ImageItem.js
+++ components/ImageItem.js
@@ -4,34 +4,34 @@ import { StyleSheet, View, Text, Image } from 'react-native'
 
 export default function Imageitem(image) {
 
- const img = image.image;
+    const img = image.image;
  //console.log(img)
 
 return (
 <View style={styles.main_container}>
- <Image style={styles.image} source={{ uri: img.webformatURL }}/>
- <Text style={styles.title_text}>Id :{img.id}</Text>
- <Text style={styles.title_text}>tags :{img.tags}</Text>
- <Text style={styles.title_text}>downloads :{img.downloads}</Text>
+    <Image style={styles.image} source={{ uri: img.webformatURL }}/>
+    <Text style={styles.title_text}>Id :{img.id}</Text>
+    <Text style={styles.title_text}>tags :{img.tags}</Text>
+    <Text style={styles.title_text}>downloads :{img.downloads}</Text>
 </View>
 );
 }
 
 
 const styles = StyleSheet.create({
- main_container: {
- marginLeft: 10,
+    main_container: {
+    marginLeft: 10,
 marginRight: 10,
- height: 190,
- marginTop: 50,
- marginBottom: 50
- },
- title_text: {
- fontSize:20
- },
- image: {
-   width: '100%',
-   height: '80%',
-   borderRadius: 3, 
- }
+    height: 190,
+    marginTop: 50,
+    marginBottom: 50
+    },
+    title_text: {
+    fontSize:20
+    },
+    image: {
+    width: '100%',
+    height: '80%',
+    borderRadius: 3, 
+    }
 })
--- components/Search.js
+++ components/Search.js
@@ -4,39 +4,39 @@ import { StyleSheet, View, TextInput, Button, FlatList, Text} from 'react-native
 import Imageitem from './ImageItem';
 import GetPictures from './Pixabay';
 export default function Search() {
- const [motrecherche, setMotrecherche] = useState();
- const [data_img, setData_img] = useState();
- return (
- <View style={styles.container}>
- <TextInput
- onChangeText={(text) => setMotrecherche(text)}
- value={motrecherche}
- style={styles.textinput}
- PlaceHolder="Mots Clefs"
- />
- <Button 
- title="Rechercher"
- onPress={() => {
- GetPictures(motrecherche).then((rep) => {
- setData_img(rep.hits);
- });
- }}
- />
- <FlatList
- data={data_img}
- keyExtractor={(item) => item.id.toString()}
- renderItem={({ item }) => <Imageitem image={item} />}
- />
- </View>
- );
-}
-const styles = StyleSheet.create({
- container: {
- marginTop: 20,
- },
- textinput: {
- borderRadius: 5,
- borderWidth: 1,
- height: 30,
- },
+    const [motrecherche, setMotrecherche] = useState();
+    const [data_img, setData_img] = useState();
+    return (
+    <View style={styles.container}>
+    <TextInput
+    onChangeText={(text) => setMotrecherche(text)}
+    value={motrecherche}
+    style={styles.textinput}
+    PlaceHolder="Mots Clefs"
+    />
+    <Button 
+    title="Rechercher"
+    onPress={() => {
+    GetPictures(motrecherche).then((rep) => {
+    setData_img(rep.hits);
+    });
+    }}
+    />
+    <FlatList
+    data={data_img}
+    keyExtractor={(item) => item.id.toString()}
+    renderItem={({ item }) => <Imageitem image={item} />}
+    />
+    </View>
+    );
+    }
+    const styles = StyleSheet.create({
+    container: {
+    marginTop: 20,
+    },
+    textinput: {
+    borderRadius: 5,
+    borderWidth: 1,
+    height: 30,
+    },
 });
