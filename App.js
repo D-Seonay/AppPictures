fff3f7845aa5e9bdc762dc72678a43b2c14f7dff
Mathéo DELAUNAY <dseonay@gmail.com>
2023-11-16 17:27:24 +0100

    Add navigation and refactor code

--- App.js
+++ App.js
@@ -1,37 +1,10 @@
-
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, Button, FlatList, Image } from 'react-native';
-import Search from './components/Search';
-import ImageItem from './components/ImageItem';
-import GetPictures from './components/Pixabay';
-
-
+// Search.js
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Search from './components/Search'
+import Nav from './NAV/nav'
 export default function App() {
-  const [images, setImages] = useState([]);
-
-  const searchImages = async (searchTerm) => {
-    const results = await GetPictures.searchImages(searchTerm);
-    setImages(results);
-  };
-
-  return (
-    <View style={styles.container}>
-      <Search onSearch={searchImages} />
-      <FlatList
-        data={images}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => <ImageItem image={item} />}
-      />
-    </View>
-  );
+return (
+  <Nav />
+);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 50,
-  },
-});
