6b6fd91785436612ae7cb7d5b640f9525029bbf8
Mathéo DELAUNAY <dseonay@gmail.com>
2023-11-16 16:44:29 +0100

    Refactor code to use Pixabay API and display images in a FlatList

--- App.js
+++ App.js
@@ -1,32 +1,37 @@
 
 import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList, Image } from 'react-native';
 import Search from './components/Search';
 import ImageItem from './components/ImageItem';
-import GetPictures  from './components/pixabay';
+import GetPictures from './components/Pixabay';
+
 
 export default function App() {
-  const [results, setResults] = useState([]);
+  const [images, setImages] = useState([]);
 
-  const handleSearch = async (query) => {
-    const data = await GetPictures(query);
-    setResults(data.hits);
-  };
-
-  const renderResults = () => {
-    return results.map((result) => (
-      <ImageItem key={result.id} image={result} />
-    ));
+  const searchImages = async (searchTerm) => {
+    const results = await GetPictures.searchImages(searchTerm);
+    setImages(results);
   };
 
   return (
-    <View>
-      <Search onSearch={handleSearch} />
-      {results.length > 0 ? (
-        renderResults()
-      ) : (
-        <Text>No results found</Text>
-      )}
+    <View style={styles.container}>
+      <Search onSearch={searchImages} />
+      <FlatList
+        data={images}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => <ImageItem image={item} />}
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 50,
+  },
+});
